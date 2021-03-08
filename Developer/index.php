f4faf008b54856a6c5357fb88357ddcca9be08cb
rlawodus70 <rlawodus70@naver.com>
2021-03-08 10:40:55 +0900

    index-page-process(1)

---
+++ Developer/index.php
@@ -0,0 +1,32 @@
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <title>Sensetive</title>
+    <link rel="icon" type="image/png" sizes="48x48" href="image/icon/favicon.png">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/common.css">
+</head>
+<body>
+    <!-- 모달 박스 -->
+    <div id ="modal-box">
+    </div>
+
+    <!-- 메뉴바 및 로고  -->
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    
+    <!-- 이미지 슬라이드 구역 -->
+    <div id="main-imgslide">
+    </div>
+
+    <!-- 컨텐츠 구역 -->
+    <div id="index-content">
+    </div>
+
+    <!-- 푸터 영역 -->
+    <footer></footer>
+</body>
+</html>
